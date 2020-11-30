5a253b8885c8b53a1a2e9f1dde8a9c6956da561c
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2020-11-29 21:25:20 -0300

    Correção de bug no formatador de json e backup de receitas

--- projMisc/format_json.py
+++ projMisc/format_json.py
@@ -7,22 +7,27 @@ r_file.close()
 
 #print(j_object[0]["ingredients"].replace("['", "").replace("']", '').split("', '"))
 
-
+# x = 0
 for recipe in j_object:
-    if type(recipe["ingredients"]) == str:
-        recipe["ingredients"] = recipe["ingredients"].replace("['", "").replace("']", '').split("', '")
+    # if x== 1: break
+    # x += 1
+
+    # print(recipe["tags"])
+    # if type(recipe["ingredients"]) == str:
+    #     recipe["ingredients"] = recipe["ingredients"].replace("['", "").replace("']", '').split("', '")
 
 
-    if type(recipe["cooking_method"]) == str:
-        recipe["cooking_method"] = recipe["cooking_method"].replace("['", "").replace("']", '').split("', '")
+    # if type(recipe["cooking_method"]) == str:
+    #     recipe["cooking_method"] = recipe["cooking_method"].replace("['", "").replace("']", '').split("', '")
 
-    if type(recipe["tags"]) == str:
-        recipe["tags"] = recipe["tags"].split(", ")
+    if ',' in recipe["tags"][0]:
+        # print(">>>", recipe["tags"][0].split(","))
+        recipe["tags"] = recipe["tags"][0].split(",")
 
-    if type(recipe["cuisine"]) == str:
-        recipe["cuisine"] = recipe["cuisine"].replace("['", "").replace("']", '').split(", ")
+    # if type(recipe["cuisine"]) == str:
+    #     recipe["cuisine"] = recipe["cuisine"].replace("['", "").replace("']", '').split(", ")
 
         
-        w_file = open("recipes.json", "w")
+w_file = open("recipes.json", "w")
 json.dump(j_object, w_file)
 w_file.close()
